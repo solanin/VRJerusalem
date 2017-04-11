bcc11ab0d97bb26a4071b385481949c0f2da3045
Jacob Westerback <sjc5283@g.rit.edu>
2017-04-11 16:52:56 -0400

    Added AdjustVRCameras Script

---
+++ VRJerusalem/Assets/JVR/Scripts/AdjustVRCameras.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustVRCameras : MonoBehaviour {
+
+    private bool hasUpdated;
+    public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        hasUpdated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasUpdated){
+            Camera[] cameras = GameObject.FindObjectsOfType<Camera>();
+            for(int i = 0; i < cameras.Length; i++)
+            {
+                if (cameras[i].name == "Camera Left")
+                {
+					cameras[i].cullingMask = (1 << LayerMask.NameToLayer("Left"));
+					cameras [i].stereoTargetEye = StereoTargetEyeMask.Left;
+				}
+
+                if (cameras[i].name == "Camera Right")
+                {
+					cameras[i].cullingMask = (1 << LayerMask.NameToLayer("Right"));
+					cameras[i].stereoTargetEye = StereoTargetEyeMask.Right;
+                }
+            }
+            hasUpdated = true;
+        }
+	}
+}
